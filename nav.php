8c07631a88a9a9b8310ae6c2636450f1c1b232e3
arguiot <arguiot@gmail.com>
2016-08-19 17:25:22 +1000

    added nav.php

---
+++ nav.php
@@ -0,0 +1,15 @@
+<nav>
+  <h2>Articles</h2>
+  <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" class="menu">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+  </svg>
+  <ul class="nav">
+   <li class="menu-youtube"><a href="https://www.youtube.com/user/TheSientist"><span>YouTube</span></a></li>
+<li class="menu-a-propos"><a href="https://www.the-scientist.fr/about/"><span>À propos</span></a></li>
+<li class="menu-search"><a href="https://www.the-scientist.fr/search/"><span>Search</span></a></li>
+<li class="menu-videos"><a href="https://www.the-scientist.fr/videos/"><span>Vidéos</span></a></li>
+<li class="menu-contact"><a href="https://www.the-scientist.fr/contact/"><span>Contact</span></a></li>
+  </ul>
+</nav>
+
