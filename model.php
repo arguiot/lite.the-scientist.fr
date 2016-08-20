845bb8afd580158e5d2f64e041f9cf881dc895a2
arguiot <arguiot@gmail.com>
2016-08-20 12:44:30 +1000

    updated model.php

--- model.php
+++ model.php
@@ -15,19 +15,23 @@
 <div class="articles-desktop">
 <div class="apple-articles">
 <?php
-$sql = "SELECT * FROM blog_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY post_date DESC lIMIT 0, 5";
+$sql = "SELECT * FROM blog_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY post_date DESC lIMIT 1, 4";
 foreach($bdd->query($sql) as $val)
 {
   $articleTitle = $val['post_title'];
   $articleID = $val['ID'];
   $articleDate = $val['post_date'];
-  $articleDesc = htmlspecialchars(mb_strimwidth($val['post_content'], 0, 50, "..."));
+  $articleDesc = strip_tags(mb_strimwidth($val['post_content'], 0, 50, "..."));
   ?>
     
           <a href="<?php echo "article.php?id=" . $articleID; ?>" title="<?php echo $articleTitle; ?>" class="article-li apple"></a>
           <div class="article-li apple" style="background: url(<?php echo "/assets/img/" . $articleID . ".png"; ?>); background-size: cover; background-position: center;">
             <div class="content">
-              <h3><?php echo $articleTitle; ?></h3>
+              <?php if (strlen($articleTitle) > 30) {
+                 echo "<h4>";
+              } else {
+                echo "<h3>";
+              } echo $articleTitle; ?></h4></h3>
               <p><?php echo $articleDesc; ?></p>
               <span class="date"><?php echo $articleDate; ?></span>
               <a href="<?php echo "article.php?id=" . $articleID; ?>" title="<?php echo $articleTitle; ?>" id="read">Lire cet Article</a>
